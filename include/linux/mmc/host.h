19c0c8829768cc1e8e43d5bd83a8d348633d65ff
Giulio Cervera <giulio.cervera@gmail.com>
2012-05-01 17:24:30 +0200

    mmc: fix host header

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -321,9 +321,6 @@ struct mmc_host {
 	bool perf_enable;
 #endif
 	unsigned long		private[0] ____cacheline_aligned;
-
-	/* HTC extension */
-	struct delayed_work	remove;
 };
 
 extern struct mmc_host *mmc_alloc_host(int extra, struct device *);
